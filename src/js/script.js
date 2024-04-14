25e0df3a9adaccc08c93ec846493c03745256d79
Ramaniuk Vasili <vasili.ramaniuk.83@gmail.com>
2024-04-14 03:22:59 +0300

    fourth section created, added jquery slick slider

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg" alt="prev"/></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg" alt="next"/></button>'
+    });
+});
