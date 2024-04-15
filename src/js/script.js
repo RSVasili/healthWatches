79ade06eb6a710326e810de393a2ffa434c14844
Ramaniuk Vasili <vasili.ramaniuk.83@gmail.com>
2024-04-15 16:52:05 +0300

    replaced the slick slider with a slider written in JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,26 @@
 
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg" alt="prev"/></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg" alt="next"/></button>'
-    });
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg" alt="prev"/></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg" alt="next"/></button>'
+//     });
+// });
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    nav: false,
+    controls: false
 });
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+})
