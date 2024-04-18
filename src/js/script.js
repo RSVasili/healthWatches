fa4139cd29100555cd1e574699f232a5d4ffb28c
Ramaniuk Vasili <vasili.ramaniuk.83@gmail.com>
2024-04-18 23:54:21 +0300

    index.html has been validated. Added mailer

--- src/js/script.js
+++ src/js/script.js
@@ -55,7 +55,7 @@ $(document).ready(function () {
             $(this).on('click', function (e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                $('.catalog-item__details').eq(i).toggleClass('catalog-item__details_active');
             });
         });
     };
@@ -144,5 +144,64 @@ $(document).ready(function () {
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
+    $('form').submit(function (e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    //Smooth scroll and page up
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1200) {
+            $('.pageup').fadeIn('slow');
+        } else {
+           $('.pageup').fadeOut();
+        }
+
+    });
+
+    $("a[href^='#']").click(function () {
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    // $("a").on('click', function(event) {
+    //
+    //     // Make sure this.hash has a value before overriding default behavior
+    //     if (this.hash !== "") {
+    //         // Prevent default anchor click behavior
+    //         event.preventDefault();
+    //
+    //         // Store hash
+    //         var hash = this.hash;
+    //
+    //         // Using jQuery's animate() method to add smooth page scroll
+    //         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    //         $('html, body').animate({
+    //             scrollTop: $(hash).offset().top
+    //         }, 800, function(){
+    //
+    //             // Add hash (#) to URL when done scrolling (default click behavior)
+    //             window.location.hash = hash;
+    //         });
+    //     } // End if
+    // });
 });
 
